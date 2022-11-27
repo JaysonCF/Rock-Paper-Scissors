06c3bdfdd6d85d776ec5ea5ce96800806ffe09cf
JaysonCFe <118558905+JaysonCF@users.noreply.github.com>
2022-11-27 17:10:17 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,34 +1,37 @@
-// create function getComputerChoice to make computer play,
-// return rock, paper, or scissors randomly from computer.
-// make function that is for a single round of game with two parameters (playerSelection, computerSelection),
-// make a return string that declares winner of round function ( ex: 'You Lose! Paper beats Rock),
-// playerSelection parameter must be made case insensitive !!!
-// make function game(),
-// call the round function inside of game(),
-// 5 round game that keeps score and reports a winner or loser at the end.
- 
-const options = ['Rock', 'Paper', 'Scissors']
+const choices = ['rock', 'paper', 'scissors']
 
-function getComputerChoice(){
-    const choice = options[Math.floor(Math.random()*options.length)];
-    return choice;
+function game(){
+    //play game
+    //play 5 rounds in the game 
+    playRound();
 }
-console.log(getComputerChoice())
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == computerSelection){
-        return 'Tie'
-    }
-    else if (
-        (playerSelection == 'Rock' && computerSelection == 'Scissors')
-        (playerSelection == 'Paper' && computerSelection == 'Rock')
-        (playerSelection == 'Scissors' && computerSelection == 'Paper')
-    ){
-    return 'Player';
-    }  
-    
-    else {
-        return 'Computer'
-    }
-    
+    //round of the game 
+    playerSelection = playerChoice();
+    computerSelection = computerChoice();
 }
+
+function playerChoice(){
+    //players choice 
+    let input = prompt('Type Rock, Paper, or Scissors');
+    while (input == null) {
+        input = prompt('Type Rock, Paper, or Scissors');
+    }
+    input = input.toLowerCase();
+
+
+    console.log(input);
+}
+
+function computerChoice(){
+    //computers choice
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function validateInput(choice){
+
+}
+
+
+game();
