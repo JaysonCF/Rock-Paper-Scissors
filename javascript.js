80edc542eba09a9edfd03bf81811ba6e5b564042
JaysonCFe <118558905+JaysonCF@users.noreply.github.com>
2022-11-28 16:25:18 -0500

    Update javascript.js, Update index

--- javascript.js
+++ javascript.js
@@ -1,87 +1,12 @@
-const choices = ['rock', 'paper', 'scissors']
-const winners = [];
-
-function game(){
-    //play game
-    //play 5 rounds in the game 
-    for (i = 1; i <= 5; i ++){
-        playRound(i);
-    }
-    logWins();
+// COMPUTER RANDOM SELECTION
+let computerChoices = Math.floor(Math.random()* 3);
+let computerSelection = computerChoices;
+if (computerSelection == 0){
+    computerSelection = 'rock';
+}else if (computerSelection == 1){
+    computerSelection = 'paper';
+}else if (computerSelection == 2){
+    computerSelection = 'scissors';
 }
+console.log(computerSelection)
 
-function playRound(round){
-    //round of the game 
-    const playerSelection = playerChoice();
-    const computerSelection = computerChoice();
-    const winner = checkWinner(playerSelection, computerSelection);
-    winners.push(winner);
-    logRound(playerSelection, computerSelection, winner, round);
-}
-
-function playerChoice(){
-    //players choice 
-    let input = prompt('Type Rock, Paper, or Scissors');
-    while (input == null) {
-        input = prompt('Type Rock, Paper, or Scissors');
-    }
-    input = input.toLowerCase();
-    let check = validateInput(input);
-    while (check == false) {
-        input = prompt(
-            'Type Rock, Paper, or Scissors. Caps does not matter but spelling does!'
-        );
-        while (input == null) {
-            input = prompt('Type Rock, Paper, or Scissors');
-        }
-        input = input.toLowerCase();
-        check = validateInput(input);
-    }
-    return input;
-}
-    
-function computerChoice(){
-    //computers choice
-    return choices[Math.floor(Math.random()*choices.length)];
-}
-
-function validateInput(choice){
-    //validates the input
-    return choices.includes(choice);
-}
-
-function checkWinner(choiceP, choiceC){
-    //checks to see who won
-    if (choiceP === choiceC){
-        return 'TIE!';
-    } else if ((choiceP === 'rock' && choiceC === 'scissors') || 
-    (choiceP ==='paper' && choiceC === 'rock') || 
-    (choiceP === 'scissors' && choiceC === 'paper')){
-        return 'Player';
-    } else {
-        return 'Computer';
-    }
-}
-
-function logWins(){
-    //logs the wins of game
-    let playerWins = winners.filter((item) => item == 'Player').length;
-    let computerWins = winners.filter((item) => item == 'Computer').length;
-    let ties = winners.filter((item) => item == 'TIE!').length;
-    console.log('Results:');
-    console.log('Player Wins:', playerWins);
-    console.log('Computer Wins:', computerWins);
-    console.log('Ties:', ties);
-}
-
-function logRound(playerChoice, computerChoice, winner, round){
-    //logs the round winner
-    console.log('Round:', round);
-    console.log('Player Chose:', playerChoice);
-    console.log('Computer Chose:', computerChoice);
-    console.log(winner, 'Won the Round!');
-    console.log('--------------------------');
-}
-
-
-game();
