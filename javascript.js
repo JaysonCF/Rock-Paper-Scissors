e485ebae2c3a9e04d8737c9f41de5b8ed9969407
JaysonCFe <118558905+JaysonCF@users.noreply.github.com>
2022-11-27 22:49:42 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
 const choices = ['rock', 'paper', 'scissors']
+const winners = [];
 
 function game(){
     //play game
     //play 5 rounds in the game 
-    playRound();
+    for (i = 1; i <= 5; i ++){
+        playRound(i);
+    }
+    logWins();
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(round){
     //round of the game 
-    playerSelection = playerChoice();
-    computerSelection = computerChoice();
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
+    logRound(playerSelection, computerSelection, winner, round);
 }
 
 function playerChoice(){
@@ -19,18 +26,61 @@ function playerChoice(){
         input = prompt('Type Rock, Paper, or Scissors');
     }
     input = input.toLowerCase();
-
-
-    console.log(input);
+    let check = validateInput(input);
+    while (check == false) {
+        input = prompt(
+            'Type Rock, Paper, or Scissors. Caps does not matter but spelling does!'
+        );
+        while (input == null) {
+            input = prompt('Type Rock, Paper, or Scissors');
+        }
+        input = input.toLowerCase();
+        check = validateInput(input);
+    }
+    return input;
 }
-
+    
 function computerChoice(){
     //computers choice
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
 function validateInput(choice){
+    //validates the input
+    return choices.includes(choice);
+}
 
+function checkWinner(choiceP, choiceC){
+    //checks to see who won
+    if (choiceP === choiceC){
+        return 'TIE!';
+    } else if ((choiceP === 'rock' && choiceC === 'scissors') || 
+    (choiceP ==='paper' && choiceC === 'rock') || 
+    (choiceP === 'scissors' && choiceC === 'paper')){
+        return 'Player';
+    } else {
+        return 'Computer';
+    }
+}
+
+function logWins(){
+    //logs the wins of game
+    let playerWins = winners.filter((item) => item == 'Player').length;
+    let computerWins = winners.filter((item) => item == 'Computer').length;
+    let ties = winners.filter((item) => item == 'TIE!').length;
+    console.log('Results:');
+    console.log('Player Wins:', playerWins);
+    console.log('Computer Wins:', computerWins);
+    console.log('Ties:', ties);
+}
+
+function logRound(playerChoice, computerChoice, winner, round){
+    //logs the round winner
+    console.log('Round:', round);
+    console.log('Player Chose:', playerChoice);
+    console.log('Computer Chose:', computerChoice);
+    console.log(winner, 'Won the Round!');
+    console.log('--------------------------');
 }
 
 
