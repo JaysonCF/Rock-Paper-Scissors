88e5de393135dc896427bbd6538100d2ee3cd2eb
JaysonCFe <118558905+JaysonCF@users.noreply.github.com>
2022-11-29 15:30:11 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,10 @@
-// COMPUTER RANDOM SELECTION
-let computerChoices = Math.floor(Math.random()* 3);
-let computerSelection = computerChoices;
-if (computerSelection == 0){
-    computerSelection = 'rock';
-}else if (computerSelection == 1){
-    computerSelection = 'paper';
-}else if (computerSelection == 2){
-    computerSelection = 'scissors';
-}
-console.log(computerSelection)
+// Information "Storage"
+const options = ['rock', 'paper', 'scissors'];
 
+
+// Computer Selection 
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random()*options.length)];
+    console.log(choice);
+}
+getComputerChoice()
